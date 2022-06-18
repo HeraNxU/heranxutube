abfbb7517963434daf9820930d8b0b1f7048856f
HeraNxU <471005304@QQ.COM>
2022-06-19 01:29:14 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');
+define('APIKEY', 'AIzaSyAhPIRlhWVzKJQLlA90OLSQpaqDeYg0uwQ');
 define('GJ_CODE', 'JP');
-define('SITE_NAME', '大爷常来玩儿啊');
-define('TITLENAME', 'DouDouTube');
+define('SITE_NAME', 'heranxutube');
+define('TITLENAME', 'heranxutube');
 define('EN2DEKEY', 'lujianbupingyishenghou');
-define('EMAIL', 'xiaofufua@protonmail.com');
+define('EMAIL', 'andy471005304@gmail.com');
 ?>
